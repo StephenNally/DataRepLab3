233c9846345306ac5a87fede1ecd1f3568fc3907
Stephen Nally <g00418614@atu.ie>
2024-12-13 01:00:53 +0000

    lab4 finished

--- src/components/create.js
+++ src/components/create.js
@@ -1,18 +1,20 @@
-// create.js
-
 import { useState } from "react";
 
-function Create() {
+const Create = () => {
+
     const [title, setTitle] = useState('');
+    const [year, setYear] = useState('');
+    const [poster, setPoster] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title);
+        const movie = {title,year,poster};
+        console.log(movie);
     }
 
     return (
         <div>
-            <h2>This is my Create Component.</h2>
+            <h3>Hello from create component!</h3>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label>Add Movie Title: </label>
@@ -22,10 +24,27 @@ function Create() {
                         onChange={(e) => { setTitle(e.target.value) }}
                     />
                 </div>
-                <input type="submit" value="Add Movie" />
+                <div className="form-group">
+                    <label>Add Movie Year: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={year}
+                        onChange={(e) => { setYear(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Movie Poster: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={poster}
+                        onChange={(e) => { setPoster(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Add Movie"></input>
+                </div>
             </form>
         </div>
     );
 }
-
 export default Create;
--- src/components/read.js
+++ src/components/read.js
@@ -1,25 +1,25 @@
-//Read.js
-
 import Movies from "./movies";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function Read() {
+const Read = () => {
+
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get('<my_api_url>')
+    axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
       .then((response) => {
+        console.log(response.data);
         setMovies(response.data.movies);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  });
 
   return (
     <div>
-      <h2>This is my Read Component.</h2>
+      <h3>Hello from read component!</h3>
       <Movies myMovies={movies} />
     </div>
   );
